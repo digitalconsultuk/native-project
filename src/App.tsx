11128d4a5a7aed6b054248ef13add0ce6060be7a
digitalconsultuk <github@digitalconsult.uk>
2026-04-07 22:53:35 +0100

    feat(contact): added contact component (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 
-import {Route, Routes, Navigate} from "react-router";
+import {Route, Routes, Navigate, useLocation} from "react-router";
 import {HomePage} from "./pages/HomePage";
 import {BookingPage} from "./pages/BookingPage";
 import {HeaderComponent} from "./components/HeaderComponent";
 import MobileNavigation from "./components/MobileComponent";
 import {FooterComponent} from "./components/FooterComponent";
+import {useEffect} from "react";
+import {ContactPage} from "./pages/ContactPage.tsx";
 //import Breadcrumbs from '@mui/material/Breadcrumbs';
 
 function App() {
+   const location = useLocation();
+
+   useEffect(() => {
+      if (location.hash) {
+         setTimeout(() => {
+            const element = document.getElementById(location.hash.slice(1));
+            if (element) {
+               element.scrollIntoView({ behavior: 'smooth' });
+            }
+         }, 100);
+      } else {
+         window.scrollTo(0, 0);
+      }
+   }, [location]);
+
    return (
       <>
          <HeaderComponent/>
@@ -16,6 +33,7 @@ function App() {
               <Route path="/" element={<Navigate to="/home" replace />}/>
               <Route path="/home" element={<HomePage />}/>
               <Route path="/booking" element={<BookingPage />}/>
+              <Route path="/contact" element={<ContactPage />}/>
             </Routes>
          <FooterComponent />
       </>
---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,11 @@
+//import  * as React from 'react';
+import { ContactComponent } from '../components/ContactComponent';
+
+const ContactPage = () => {
+  return (
+    <main className={'mt-10'}>
+      <ContactComponent />
+    </main>
+  );
+};
+export {ContactPage};
